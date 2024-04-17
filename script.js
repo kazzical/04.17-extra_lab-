94fb72e0593afde71632df9d1feb8f4d578a1002
kazzical <133119885+kazzical@users.noreply.github.com>
2024-04-17 21:17:49 +0000

    learned to change background colors

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ function applyColor() {
     let backgroundColor;
 
     // write your conditional if/else statements here:
-
+if (color === "red"){
+  backgroundColor = "red";
+} else if (color === "green"){
+  backgroundColor = "green";
+}else if (color === "orange"){
+    backgroundColor = "orange";
+  }else if (color === "yellow"){
+    backgroundColor = "yellow";
+  }else if (color === "blue"){
+    backgroundColor = "blue";
+  }else if (color === "purple"){
+    backgroundColor = "purple";
+  }
 
     document.body.style.backgroundColor = backgroundColor;
 }
