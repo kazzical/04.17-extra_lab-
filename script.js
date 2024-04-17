1387f25fd95c85159ed0c19000f770d83749c0e5
Magdalena May Gero <114107908+magdalenamaygero@users.noreply.github.com>
2024-04-17 03:43:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function applyColor() {
+    const color = document.getElementById("color").value;
+    let backgroundColor;
+
+    // write your conditional if/else statements here:
+
+
+    document.body.style.backgroundColor = backgroundColor;
+}
